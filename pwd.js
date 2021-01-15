4c883c6ac3ac2fafd0cb1d55c813c227a6489c83
Adam Kopell <aakopell@gmail.com>
2021-01-15 10:10:17 -0800

    coded up till modules - creating a module

---
+++ pwd.js
@@ -0,0 +1,8 @@
+module.exports = function () {
+  process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    if (cmd === 'pwd') {
+      process.stdout.write(process.cwd());
+    }
+  });
+};
